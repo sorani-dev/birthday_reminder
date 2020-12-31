e03518c599d6c8aa88c0b034945248a65d7db451
Sorani Riku <sorani.dev@outlook.com>
2020-12-31 16:45:15 +0100

    Display the new UI: Display the people details in List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,10 +4,19 @@ const List = (props) => {
 	const { people } = props
 	return (
 		<>
-			{people.map(person => (
-				<p key={person.id}>{person.name}</p>
-
-			))}
+			{people.map(person => {
+				const {id, name, image, age} = person
+				return (
+				<article key={id} className="person">
+					<img src={image} alt={name} />
+					<div>
+					{/* <p key={id}>{name}</p> */}
+						<h4>{name}</h4>
+						<p>{age}</p>
+					</div>
+				</article>
+				)
+			})}
 
 		</>
 	)
