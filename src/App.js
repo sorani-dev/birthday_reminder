f0bf8b51396db5930088a7063bdf9a3ece12c611
Sorani Riku <sorani.dev@outlook.com>
2020-12-31 16:56:26 +0100

    Clear all data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import List from './components/List'
 import data from './data/data.js'
 
 function App() {
+  const [people, setPeople] = useState(data)
+  const [numOfPeople, setNumOfPeople] = useState(data.length)
+
+  const clearAll = () => {
+    setPeople([])
+    setNumOfPeople(0)
+  }
+
   return (
     <main>
       <section className="container">
-          <h3>{data.length} birthdays today</h3>
-          <List people={data} />
+          <h3>{numOfPeople} birthdays today</h3>
+          <List people={people} />
+          <button value="Clear All" onClick={clearAll} />
       </section>
     </main>
   );
